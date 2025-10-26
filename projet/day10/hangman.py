1e313c413c1ad35c8c83ce088d4a645d042bc533
Enzo <wisteur@epitech.eu>
2025-10-26 12:54:33 +0400

    my site

--- projet/day10/hangman.py
+++ projet/day10/hangman.py
@@ -10,44 +10,40 @@ def mot_aleatoire():
 mot_a_deviner = mot_aleatoire()
 print(mot_a_deviner)
 
-mot_cacher = "_" * len(mot_a_deviner)
+mot_cache = "_" * len(mot_a_deviner)
 
-n=0
-lettre = ""
+n = 0
+lettres = ""
 
-while n<12:
+while n < 12:
 
-    if mot_cacher != mot_a_deviner:
+    if mot_cache != mot_a_deviner:
         os.system('cls' if os.name == 'nt' else 'clear')
 
-        print("Mot à deviner : ", mot_cacher)
-        print("nombre de vie restantant: ",12-n)
-        print("vous avé utiliser: ",lettre)
-        utilisateur = input("Entrez une lettre:")
+        print("Mot à deviner :", mot_cache)
+        print("Nombre de vies restantes :", 12 - n)
+        print("Vous avez utilisé :", lettres)
+        utilisateur = input("Entrez une lettre : ")
         
         lettre_trouvee = False
 
         for i in range(len(mot_a_deviner)):
-
             if utilisateur == mot_a_deviner[i]:
-                mot_cacher = mot_cacher[:i] + utilisateur + mot_cacher[i+1:]
+                mot_cache = mot_cache[:i] + utilisateur + mot_cache[i+1:]
                 lettre_trouvee = True
 
         if not lettre_trouvee:
             n += 1
             for i in utilisateur:
-                lettre=lettre+" "+i
+                lettres = lettres + " " + i
         
-        print(mot_cacher)
+        print(mot_cache)
 
-
-    elif mot_a_deviner == mot_cacher:
-
-        print("\033[32mBravo ! Le mot\033[0m",mot_a_deviner,"\033[32ma été trouvé\033[0m" )
+    elif mot_a_deviner == mot_cache:
+        print("\033[32mBravo ! Le mot\033[0m", mot_a_deviner, "\033[32ma été trouvé\033[0m")
         break
 
-if n>=12:
-    
+if n >= 12:
     os.system('cls' if os.name == 'nt' else 'clear')
     print("\033[31mVOUS AVEZ PERDU\033[0m")
-    print("Le mot a deviner était: ",mot_a_deviner)
+    print("Le mot à deviner était :", mot_a_deviner)
